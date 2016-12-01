d5109fef40b1f09b224ae37bb5c57045b59bf43f
Bryan Leonard <b.leonard@zoho.com>
2016-12-01 18:57:33 -0500

    Set up some state stuff.

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,32 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+	
+	constructor() {
+		super();
+
+		// bind method to app
+		this.addFish = this.addFish.bind(this);
+
+		// initial state
+		this.state = {
+			fishes: {},
+			order: {}
+		};
+	}
+
+	addFish(fish) {
+		//update state
+		const fishes = {...this.state.fishes}; //take copy every item our obj and spread it into this new obj
+		
+		// add in our new fish
+		const timestamp = Date.now();
+		fishes[`fish-${timestamp}`] = fish;
+		
+		//set state
+		this.setState({ fishes }) // {fishes: fishes}
+	}
+
 	render() {
 		return (
 			<section className="catch-of-the-day">
@@ -12,7 +38,7 @@ class App extends React.Component {
 					{/* <Fish /> */}
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={this.addFish} />
 			</section>
 		)
 	}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // let's go!
-// begin with 13 at 7:50
+// begin with 14
 
 import React from 'react';
 import { render } from 'react-dom'; // react-dom render method only, import ReactDOM from 'react-dom'; // if we wanted the whole package
