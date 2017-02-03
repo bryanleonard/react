f673917e961ed987edcfb1cbceead25d79e22714
Bryan Leonard <b.leonard@zoho.com>
2017-02-02 21:53:58 -0500

    being adding animation

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,9 @@ class App extends React.Component {
 		this.addFish     = this.addFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
 		this.addToOrder  = this.addToOrder.bind(this);
+		this.removeFromOrder  = this.removeFromOrder.bind(this);
+		this.updateFish  = this.updateFish.bind(this);
+		this.removeFish  = this.removeFish.bind(this);
 
 		// initial state
 		this.state = {
@@ -63,6 +66,18 @@ class App extends React.Component {
 		this.setState({ fishes }) // {fishes: fishes}
 	}
 
+	updateFish(key, updatedFish) {
+		const fishes = {...this.state.fishes};
+		fishes[key] = updatedFish;
+		this.setState({fishes});
+	}
+
+	removeFish(key) {
+		const fishes = {...this.state.fishes};
+		fishes[key] = null;
+		this.setState({ fishes });
+	}
+
 	loadSamples() {
 		this.setState({
 			fishes: SampleFishes
@@ -78,12 +93,17 @@ class App extends React.Component {
 		this.setState({ order });
 	}
 
+	removeFromOrder(key) {
+		const order = {...this.state.order};
+		delete order[key];
+		this.setState({order});
+	}
+
 	render() {
 		return (
 			<section className="catch-of-the-day">
 				<div className="menu">
 					<Header tagline="Fresh Seafood Market" />
-					{/* <Fish /> */}
 					<ul className="list-of-fishes">
 						{
 							// make obj an array, then map over the key
@@ -100,9 +120,16 @@ class App extends React.Component {
 				<Order 
 					fishes={this.state.fishes} 
 					order={this.state.order} 
-					parans={this.props.params} 
+					params={this.props.params} 
+					removeFromOrder={this.removeFromOrder}
 				/>
-				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes} />
+				<Inventory 
+					addFish={this.addFish} 
+					loadSamples={this.loadSamples} 
+					fishes={this.state.fishes} 
+					updateFish={this.updateFish} 
+					removeFish={this.removeFish}
+				/>
 			</section>
 		)
 	}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // let's go!
-// begin with 20, 5:30
+// begin with 22 - 5:30
 
 import React from 'react';
 import { render } from 'react-dom'; // react-dom render method only, import ReactDOM from 'react-dom'; // if we wanted the whole package
